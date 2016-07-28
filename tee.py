aa1f1df7a16960be517497d70df1d797e2e4db07
Davide Del Vento <davide.del.vento@gmail>
2016-07-28 08:47:26 -0600

    saving the hpcinstall log itself

---
+++ tee.py
@@ -0,0 +1,31 @@
+import sys
+
+class tee :
+    def __init__(self, _fd1, _fd2) :
+        self.fd1 = _fd1
+        self.fd2 = _fd2
+
+    def __del__(self) :
+        if self.fd1 != sys.stdout and self.fd1 != sys.stderr :
+            self.fd1.close()
+        if self.fd2 != sys.stdout and self.fd2 != sys.stderr :
+            self.fd2.close()
+
+    def write(self, text) :
+        self.fd1.write(text)
+        self.fd2.write(text)
+
+    def flush(self) :
+        self.fd1.flush()
+        self.fd2.flush()
+
+def overwrite_out_to(filename):
+    saved = sys.stdout
+    outputlog = open(filename, "w")
+    sys.stdout = tee(saved, outputlog)
+
+def overwrite_err_to(filename):
+    saved = sys.stderr
+    outputlog = open(filename, "w")
+    sys.stderr = tee(saved, outputlog)
+
