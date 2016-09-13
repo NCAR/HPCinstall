66bdaccb71063b0f0dd6f4897754a7625c551f90
Davide Del Vento <davide.del.vento@gmail>
2016-09-13 15:53:48 -0600

    TDD: testing the parse_data function

--- test/test_hpcinstall.py
+++ test/test_hpcinstall.py
@@ -1,4 +1,9 @@
 import hpcinstall
 
-def test_parse_config_file():
-    raise Exception("not implemented")
+def test_parse_config_data():
+    data = """scratch_tree: /glade/scratch
+sw_install_dir: /glade/apps/opt
+mod_install_dir: /glade/apps/opt/modulefiles/"""
+    expected = {"scratch_tree": "/glade/scratch", "sw_install_dir": "/glade/apps/opt", "mod_install_dir": "/glade/apps/opt/modulefiles/"}
+    parsed = hpcinstall.parse_config_data(data)
+    assert expected == parsed
