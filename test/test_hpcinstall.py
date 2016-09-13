b23068b8cc02321beb5b5cd9ff8bb26a2288618f
Davide Del Vento <davide.del.vento@gmail>
2016-09-13 16:27:33 -0600

    testing the wrong content of the file

--- test/test_hpcinstall.py
+++ test/test_hpcinstall.py
@@ -1,4 +1,5 @@
 import hpcinstall
+import pytest
 
 # not testing "print_invocation_info" since it's harmless and hard to test
 
@@ -9,3 +10,11 @@ mod_install_dir: /glade/apps/opt/modulefiles"""
     expected = {"scratch_tree": "/glade/scratch/", "sw_install_dir": "/glade/apps/opt/", "mod_install_dir": "/glade/apps/opt/modulefiles/"}
     parsed = hpcinstall.parse_config_data(data)
     assert expected == parsed
+
+    with pytest.raises(KeyError):
+        data = "useless_stuff: something"
+        hpcinstall.parse_config_data(data)
+
+    with pytest.raises(KeyError):
+        data = ""
+        hpcinstall.parse_config_data(data)
