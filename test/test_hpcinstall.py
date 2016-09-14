b4c1c7d9b27451bcd67564d6a2a219de6c2ae078
Davide Del Vento <davide.del.vento@gmail>
2016-09-14 14:28:58 -0600

    preliminary test_prepare_variables_and_warn

--- test/test_hpcinstall.py
+++ test/test_hpcinstall.py
@@ -30,7 +30,18 @@ def test_parse_installscript_filename():
         hpcinstall.parse_installscript_filename("incorrect_name-version")
 
 # not testing "parse_command_line_arguments" since it's harmless and hard to test
+
 # not testing "ask_confirmation_for" since it's trivial and hard to test
+# note below the
+#    hpcinstall.ask_confirmation_for = lambda x, y: str(x) + str(y)
 
 def test_get_variables():
     raise Exception("Too complicated method to test. Simplify?")
+
+def test_prepare_variables_and_warn():
+    # this method is trivial, the only things to test are:
+    # 1. make sure that what it prints is what it sets (just refactoring may suffice, no testing)
+    # 2. making sure the pass_env has it all
+    hpcinstall.ask_confirmation_for = lambda x, y: str(x) + str(y)
+    hpcinstall.prepare_variables_and_warn("/glade/apps/opt", "/glade/apps/modulefiles", None, "build-mysoftware-1.2.3")
+    raise Exception()
