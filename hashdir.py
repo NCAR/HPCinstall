e2edecf3491029f0c87f2fcad2dc2825baa9af2e
Davide Del Vento <davide.del.vento@gmail>
2016-10-06 17:42:28 -0600

    hash library basics

---
+++ hashdir.py
@@ -0,0 +1,22 @@
+import hashlib, os
+
+def hash(directory, verbose=False):
+    SHAhash = hashlib.md5()
+    for root, dirs, files in os.walk(directory):
+        for names in files:
+            filepath = os.path.join(root, names)
+            if verbose:
+                print 'Hashing', filepath
+            try:
+                f1 = open(filepath, 'rb')
+                while 1:
+                # Read file in as little chunks
+                    buf = f1.read(4096)
+                    if not buf : break
+                    SHAhash.update(hashlib.md5(buf).hexdigest())
+
+            finally:
+              f1.close()
+              SHAhash.update(hashlib.md5(filepath).hexdigest())
+
+    return SHAhash.hexdigest()
