2184371d5caef7be76767a8d33df6899a8a700d2
Davide Del Vento <davide.del.vento@gmail>
2017-02-10 11:01:58 -0700

    porting to make it work from another directory

--- show_latest.sh
+++ show_latest.sh
@@ -4,14 +4,15 @@
 
 if [[ $# -ne 1 ]]; then
     echo "Usage: $0 <directory>"
+    echo "       where directory is e.g. ~/github/ys-install-scripts"
     exit 1
 fi
 
 stuff=""
-for software in `ls --color=no -d $1`; do
+for software in `ls --color=no $1`; do
     if [[ -d $software ]]; then
          more=$( for dir in $( ls --color=no $software ); do echo $dir; done | tail -1 )
          stuff="$stuff $software/$more"
     fi
 done
-tree -ifp $stuff  | grep '^\[-' | awk '{print $2}'
+tree -ifp $1/$stuff  | grep '^\[-' | awk '{print $2}'
